124e02956a9f4bcc3556c799fe3623a8dfa7ae66
mirth_fool <81645133+mirthfool@users.noreply.github.com>
2025-03-28 10:58:32 +1300

    Remove spring boot

--- lazy.lua
+++ lazy.lua
@@ -10,10 +10,6 @@ return {
 		'neovim/nvim-lspconfig',
 		'mfussenegger/nvim-dap',
 		{
-			'JavaHello/spring-boot.nvim',
-			commit = '218c0c26c14d99feca778e4d13f5ec3e8b1b60f0',
-		},
-		{
 			'williamboman/mason.nvim',
 			-- opts = {
 			-- registries = {
